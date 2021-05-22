5b3934a91db673fa1d5bf1bfe6e71656ce9a04af
e-gunn <gunn816@hotmail.com>
2021-05-21 21:12:40 -0700

    Finish adding user interactivity

--- src/NotesList.js
+++ src/NotesList.js
@@ -2,9 +2,12 @@ import React from "react";
 import Note from "./Note.js";
 
 const keepSearchMatches = (note) => note.doesMatchSearch;
-const renderNote = (note) => <Note note={note} key={note.id} />;
 
 const NotesList = (props) => {
+  const renderNote = (note) => (
+    <Note note={note} key={note.id} onType={props.onType} />
+  );
+
   const searchMatches = props.notes.filter(keepSearchMatches);
   const noteElements = searchMatches.map(renderNote);
   return <ul className="notes-list">{noteElements}</ul>;
